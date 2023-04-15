b6091c9fdfab3ec668cf6aab2c801df4c517f89f
perry1224 <perry.w1224@gmail.com>
2023-04-14 22:35:56 -0400

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+AOS.init({
+  offset: 400, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000 // values from 0 to 3000, with step 50ms
+});
