44c4526fd4238d9cda93f09f19b29d5a3c087f0c
perry1224 <perry.w1224@gmail.com>
2023-04-17 09:56:55 -0400

    toggle switch

--- script/script.js
+++ script/script.js
@@ -16,3 +16,8 @@ AOS.init({
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000 // values from 0 to 3000, with step 50ms
 });
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
